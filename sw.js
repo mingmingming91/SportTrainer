19c082db34bf72b16e794b5141ffab433462cf47
mingmingming91 <shu0913@gmail.com>
2026-06-01 17:37:29 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
 const CACHE_NAME = 'command-player-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './script.js',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  'index.html',
+  'script.js',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
 ];
 
 // 安裝 Service Worker 並快取檔案
